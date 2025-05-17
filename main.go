367563665ca41c0d6367533ca37f5ef4108a6cb8
Aniket Pathak <aniketpathak028@gmail.com>
2025-05-17 14:35:33 +0000

    create a simple proxy server

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/aniketpathak028/proxy-go/cmd/proxy"
+)
+
+func main() {
+	port := flag.String("port", "8080", "Port to run the proxy on")
+	flag.Parse()
+
+	p := proxy.NewProxy()
+
+	server := &http.Server{
+		Addr:    ":" + *port,
+		Handler: p,
+	}
+
+	log.Printf("Starting proxy server on :%s", *port)
+	log.Printf("Configure your browser to use http://localhost:%s as HTTP proxy", *port)
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
